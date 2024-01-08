69ed24e15a72a349515f33bfcd3fce0152960cdc
Lily McCabe <lily@lilymccabe.ca>
2024-01-08 12:35:34 -0500

    Work on settings selection

--- src/render/instance_create.rs
+++ src/render/instance_create.rs
@@ -19,6 +19,12 @@ impl Renderer {
         lib: Arc<VulkanLibrary>,
         window: Arc<winit::window::Window>,
     ) -> Result<Arc<vk::instance::Instance>, renderer_error::RendererError> {
+        let wanted_extensions = vk::instance::InstanceExtensions {
+            ext_surface_maintenance1: true,
+            // ext_swapchain_colorspace: todo!(),
+            ..Default::default()
+        };
+
         let needed_extensions = vk::swapchain::Surface::required_extensions(window.as_ref());
 
         let needed_extensions = vk::instance::InstanceExtensions {
@@ -29,8 +35,8 @@ impl Renderer {
         .union(&needed_extensions);
 
         let mut info = vk::instance::InstanceCreateInfo::application_from_cargo_toml();
-        info.enabled_extensions = needed_extensions;
-
+        info.enabled_extensions =
+            needed_extensions.union(&wanted_extensions.intersection(lib.supported_extensions()));
         Ok(vk::instance::Instance::new(lib, info)?)
     }
 }
